0d35b21a96bd1cef543a19b772c5142d7a1a50ed
pacokwon <haechank@gmail.com>
2020-04-23 12:41:21 +0000

    add - read dotenv file using webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-    devServer: {
-        port: 3000,
-        host: '0.0.0.0',
-        public: 'kong.sparcs.org:7300'
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new Dotenv()
+  ],
+  devServer: {
+    port: 3000,
+    host: '0.0.0.0',
+    public: 'kong.sparcs.org:7300'
+  }
 }
