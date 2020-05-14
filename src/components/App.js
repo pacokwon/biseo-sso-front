98c767b99e0678b5d4c9cb7f708c50d1c890d324
pacokwon <haechank@gmail.com>
2020-05-14 14:38:27 +0000

    update: organize routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,21 @@ import {
   Route
 } from 'react-router-dom'
 import Login from './Login'
-// import LoginCallback from './LoginCallback'
+import LoginRedirect from './LoginRedirect'
+import LoginCallback from './LoginCallback'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login">
-          <Login />
+        <Route exact path="/login/redirect">
+          <LoginRedirect />
         </Route>
         <Route exact path="/login/callback">
-          {/* <LoginCallback /> */}
+          <LoginCallback />
+        </Route>
+        <Route path="/">
+          <Login />
         </Route>
       </Switch>
     </Router>
